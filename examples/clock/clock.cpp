c8e86c48437177a8f092de8ccbed8c0ca5eeef73
XavierP <tornado3019@gmail.com>
2026-02-07 14:22:51 +1100

    Displaying an exclamation mark

---
+++ examples/clock/clock.cpp
@@ -0,0 +1,236 @@
+// A little test program to mess around with functions, and understand how the
+// libraries work.
+
+// COMPILE TIME ERRORS
+#ifndef BOARD_HAS_PSRAM
+#error "Please enable PSRAM, Arduino IDE -> tools -> PSRAM -> OPI !!!"
+#endif
+
+// LIBRARIES
+#include <Arduino.h> // Not sure yet if this is necessary, will check back later
+// Epaper
+#include "epd_driver.h"
+#include "firasans.h"
+#include "fonts/FontReg36.h"
+#include "utilities.h"
+
+// NTP
+#include "sntp.h"
+#include "time.h"
+#include <WiFi.h>
+
+// CONSTS
+// const char* ssid       = "Dah0use";
+// const char* password   = "Eto nash durackij wpA kluchik";
+
+const char *ssid = "McDonkeys_2_4";
+const char *password = "mdqfyephjb";
+
+const char *ntpServer1 = "au.pool.ntp.org";
+const char *ntpServer2 = "pool.ntp.org";
+const long gmtOffset_sec = 3600 * 10;
+const int daylightOffset_sec = 3600;
+
+const char *time_zone = "AEST-10AEDT,M10.1.0,M4.1.0/3"; // Melbourne Time Zone
+const GFXfont *font = &FiraSans;
+// Based on font_properties_default()
+const FontProperties font_props{
+    .fg_color = 0,
+    .bg_color = 15,
+    .fallback_glyph = 0,
+    .flags = 0,
+};
+
+bool need_to_disconnect = false;
+
+// VARS
+// Starting Cursor Position
+int32_t cursor_x = 20;
+int32_t cursor_y = 160;
+
+// Frame buffer
+uint8_t *framebuffer;
+
+// Starting Time
+// inital_hour = &timeinfo.tm_hour;
+// inital_min = &timeinfo.tm_min;
+// inital_sec = &timeinfo.tm_sec;
+
+// FUNCTIONS
+
+// Prints the time saved on the ESP's RTC
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) { // Write into time struct using the ESP's
+                                  // local RTC time?
+    Serial.println("No time available (yet)");
+    return;
+  }
+
+  // Print to serial
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+  // Clear display
+  epd_clear();
+  delay(100); // Wait a bit for the display to clear
+
+  //   // Create buffer for time string
+  //   char buffer[64]; // adjust size as needed
+  //   // strftime(buffer, sizeof(buffer), "%A, %B %d %Y %H:%M:%S", &timeinfo);
+  //   strftime(buffer, sizeof(buffer), "%H:%M", &timeinfo); // Succinct time
+  //   format
+
+  // // Find the text's center position
+  // int32_t base_pos = 0;
+  // int32_t x = 0, y = 0, w = 0, h = 0;
+
+  // get_text_bounds(font, buffer, &base_pos, &base_pos, &x, &y, &w, &h,
+  // &font_props); Serial.println("Text bounds:"); Serial.printf("x: %d, y: %d,
+  // w: %d, h: %d\n", x, y, w, h);
+
+  // cursor_x = (EPD_WIDTH - w) / 2;
+  // cursor_y = (EPD_HEIGHT) / 2;
+
+  // // Print to display
+  // writeln(font, buffer, &cursor_x, &cursor_y, NULL); // Note: writeln will
+  // update cursor_x and cursor_y
+
+  // //Print out contents of the buffer (print running totals i.e., 16 FF, 1000
+  // 00, etc.) uint8_t prev_byte = 0xFF; int count = 0; for (int i = 0; i <
+  // EPD_WIDTH * EPD_HEIGHT / 2; i++)
+  // {
+  //     if (framebuffer[i] != prev_byte)
+  //     {
+  //         Serial.printf("0x%02X: %d\n", framebuffer[i], count);
+  //         count = 1;
+  //         prev_byte = framebuffer[i];
+  //     }
+  //     else
+  //     {
+  //         count++;
+  //     }
+  // }
+
+  // Rect_t area = {
+  //     .x = x1,
+  //     .y = *cursor_y - h + baseline_height,
+  //     .width = w,
+  //     .height = h
+  // };
+
+  memset(framebuffer, 255, EPD_WIDTH * EPD_HEIGHT / 2);
+
+  // TEST1
+  //   framebuffer[0] = 0x00;
+  //   framebuffer[2] = 0x00;
+  //   framebuffer[4] = 0x00;
+  //   framebuffer[6] = 0x00;
+  //   framebuffer[8] = 0x00;
+  //   framebuffer[10] = 0x00;
+  //   framebuffer[12] = 0x00;
+
+  // TEST2
+  // framebuffer[0] = 0x01;
+
+  // TEST3
+  const int frame_size = 108;
+  const int char_width = 22;
+  const int char_height = 36;
+
+  Rect_t area = {
+      .x = 50,
+      .y = 50,
+      .width = char_width,
+      .height = char_height,
+  };
+
+  // Copy an '!' to the buffer
+  for (int row = 0; row < char_height; row++) {
+    for (int col = 0; col < char_width; col++) {
+      int char_index = 1; // '!' character index in the font
+      int byte_index = (char_index * frame_size) +
+                       (row * ((char_width + 7) / 8)) + (col / 8);
+      int bit_index = 7 - (col % 8);
+      uint8_t byte = Font36_Table[byte_index];
+      uint8_t bit = (byte >> bit_index) & 0x01;
+      if (bit) {
+        int buffer_index = (row * ((char_width + 1) / 2)) + (col / 2);
+        framebuffer[buffer_index] &=
+            ~(0x80 >> (col % 2)); // Set the corresponding bit to 0 (black)
+      }
+    }
+  }
+  epd_draw_image(area, framebuffer, DrawMode_t::BLACK_ON_WHITE);
+}
+
+// Callback function (get's called when time adjusts via NTP)
+void timeavailable(struct timeval *t) {
+  Serial.println("Got time adjustment from NTP!");
+  printLocalTime();
+  need_to_disconnect = true;
+}
+
+void setup() {
+  // Serial Init
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+
+  // Epaper Display Init
+  epd_init();
+
+  // Allocate memory for frame buffer
+  framebuffer =
+      (uint8_t *)ps_calloc(sizeof(uint8_t), EPD_WIDTH * EPD_HEIGHT / 2);
+  if (!framebuffer) {
+    Serial.println("alloc memory failed !!!");
+    while (1)
+      ;
+  }
+  memset(framebuffer, 0xFF, EPD_WIDTH * EPD_HEIGHT / 2);
+
+  // Reset Display
+  epd_poweron();
+  epd_clear();
+
+  // NTP Init
+  sntp_set_time_sync_notification_cb(
+      timeavailable);      // set notification call-back function
+  sntp_servermode_dhcp(1); // (optional)
+  // configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2); //
+  // Manual daylight savings offset
+  configTzTime(time_zone, ntpServer1,
+               ntpServer2); // (Hopefully) Automatic daylight savings offsetting
+
+  // Connect to WiFi
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+
+  // Get and print time for the first time
+  printLocalTime();
+
+  // Calculate and await the initial syncing interval
+
+  // Done
+  Serial.println("Setup Complete!");
+}
+
+void loop() {
+  if (need_to_disconnect) {
+    WiFi.disconnect(true);
+    Serial.println("WiFi Disconnected!");
+    need_to_disconnect = false;
+  }
+
+  delay(60000);
+  printLocalTime(); // it will take some time to sync time :)
+
+  // Serial.println("Starting light sleep");
+  // esp_light_sleep_start();
+  // Serial.println("Exit light sleep");
+}
---
+++ src/fonts/fonts.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+struct sFONT {
+  const uint8_t *table;
+  uint16_t width;
+  uint16_t height;
+};
