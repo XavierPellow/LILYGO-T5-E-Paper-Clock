bcc56f30a397e5721da41d8a6e178583da1d2a3f
XavierP <tornado3019@gmail.com>
2026-02-07 15:04:50 +1100

    Displaying large text

--- examples/clock/clock.cpp
+++ examples/clock/clock.cpp
@@ -9,7 +9,7 @@
 // LIBRARIES
 // Epaper
 #include "epd_driver.h"
-#include "fonts/FontReg36.h"
+#include "fonts/FontReg288.h"
 #include "utilities.h"
 
 // NTP
@@ -31,13 +31,13 @@ const int daylightOffset_sec = 3600;
 
 const char *time_zone = "AEST-10AEDT,M10.1.0,M4.1.0/3"; // Melbourne Time Zone
 
-bool need_to_disconnect = false;
+bool got_time_adjustment = false;
 uint8_t *framebuffer;
 
 // Font info
-const sFONT &font = Font36;     // From FontReg36.h
-const int bytes_per_char = 108; // Manually calculated from looking at the font
-                                // file (108 bytes per character)
+const sFONT &font = Font288; // From FontReg288.h
+const int bytes_per_char = sizeof(Font288_Table) / 95;
+// 95 characters in the font file (from 0x20 to 0x7E)
 
 void copyCharToBuffer(char c, size_t x, size_t y, size_t buf_width,
                       size_t buf_height, uint8_t *buffer) {
@@ -50,7 +50,7 @@ void copyCharToBuffer(char c, size_t x, size_t y, size_t buf_width,
   const size_t font_bytes_per_row = (font.width + 7) >> 3;
   const size_t fb_bytes_per_row = (buf_width + 1) >> 1;
 
-  const uint8_t *glyph = &Font36_Table[char_index * bytes_per_char];
+  const uint8_t *glyph = &font.table[char_index * bytes_per_char];
 
   for (size_t row = 0; row < font.height; row++) {
     size_t dst_y = y + row;
@@ -87,6 +87,15 @@ void copyCharToBuffer(char c, size_t x, size_t y, size_t buf_width,
   }
 }
 
+void copyStringToBuffer(const char *str, size_t x, size_t y, size_t buf_width,
+                        size_t buf_height, uint8_t *buffer) {
+  while (*str) {
+    copyCharToBuffer(*str, x, y, buf_width, buf_height, buffer);
+    x += font.width;
+    str++;
+  }
+}
+
 // Prints the time saved on the ESP's RTC
 void printLocalTime() {
   struct tm timeinfo;
@@ -112,26 +121,16 @@ void printLocalTime() {
   Rect_t area = {
       .x = 50,
       .y = 50,
-      .width = font.width * 4,
+      .width = font.width * 5,
       .height = font.height,
   };
 
-  copyCharToBuffer('a', 0, 0, font.width * 4, font.height, framebuffer);
-  copyCharToBuffer('b', font.width * 1, 0, font.width * 4, font.height,
-                   framebuffer);
-  copyCharToBuffer('c', font.width * 2, 0, font.width * 4, font.height,
-                   framebuffer);
-  copyCharToBuffer('d', font.width * 3, 0, font.width * 4, font.height,
-                   framebuffer);
+  copyStringToBuffer(timestr, 0, 0, font.width * 5, font.height, framebuffer);
   epd_draw_image(area, framebuffer, DrawMode_t::BLACK_ON_WHITE);
 }
 
 // Callback function (get's called when time adjusts via NTP)
-void timeavailable(struct timeval *t) {
-  Serial.println("Got time adjustment from NTP!");
-  printLocalTime();
-  need_to_disconnect = true;
-}
+void timeavailable(struct timeval *t) { got_time_adjustment = true; }
 
 void setup() {
   // Serial Init
@@ -188,10 +187,12 @@ void setup() {
 }
 
 void loop() {
-  if (need_to_disconnect) {
+  if (got_time_adjustment) {
+    Serial.println("Got time adjustment from NTP!");
+    printLocalTime();
     WiFi.disconnect(true);
     Serial.println("WiFi Disconnected!");
-    need_to_disconnect = false;
+    got_time_adjustment = false;
   }
 
   delay(60000);
